8b07735455e8903ae77c408d80f29868448ea95f
Erick Nevarez <a357664@uach.mx>
2024-04-09 16:48:36 -0600

    << Se actualizó el documento app.sh y se añadio el archivo readInfo.sh>>

--- Proyecto_primer_parcial/app.sh
+++ Proyecto_primer_parcial/app.sh
@@ -2,8 +2,20 @@
 source mainMenu.sh
 source subMenu.sh
 source addInfo.sh
+source readInfo.sh
 mainMenu $1
 echo -n ">>"
 read option
 subMenu $1 $option
-addInfo $1 $option
+echo -n ">>"
+read option2
+
+
+case $option2 in 
+    1 )
+    addInfo $1 $option
+        ;;
+    4)
+    readInfo $1 $option
+        ;;
+esac
---
+++ Proyecto_primer_parcial/readInfo.sh
@@ -0,0 +1,10 @@
+source mainMenu.sh
+
+readInfo(){
+    if [ ${1,,} = "-a" ]; then
+    sectionsName="${sectionsA[$2 -1]}"
+    elif [ ${1,,} = "-t" ]; then
+    sectionsName="${sectionsT[$2 -1]}"
+    fi
+    cat ${sectionsName}.txt
+}
